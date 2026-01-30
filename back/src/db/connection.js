5d61565531fb023ac735abb65a307890f24e1a69
untitled <rayan.haj-mohamed@laplateforme.io>
2026-01-30 12:20:11 +0100

    Fix The back

--- back/src/db/connection.js
+++ back/src/db/connection.js
@@ -1,9 +1,18 @@
 import { Sequelize } from "sequelize";
 
+<<<<<<< Updated upstream
 const sequelize = new Sequelize("marsai", "root", "rootroot", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
+=======
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect:process.env.DB_DIALECT,
+>>>>>>> Stashed changes
 });
 
 sequelize.sync().then(() => {
